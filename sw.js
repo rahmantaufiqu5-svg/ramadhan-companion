de389db57e9419b3f37b1946f962557edbed386c
rahmantaufiqu5-svg <rahmantaufiqu5@gmail.com>
2026-03-03 21:00:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,20 +6,19 @@ const assets = [
   "/js/main.js",
   "/js/checklist.js",
   "/js/tilawah.js",
-  "/js/theme.js",
-  "/js/prayer.js",
   "/js/muhasabah.js",
-  "/js/stats.js"
+  "/js/stats.js",
+  "/js/theme.js"
 ];
 
-self.addEventListener("install", e=>{
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(cacheName).then(cache=>cache.addAll(assets))
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(resp=>resp||fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
