ce5d6808bf19b52ed7fbf66f5165f2699ba1db2f
rahmantaufiqu5-svg <rahmantaufiqu5@gmail.com>
2026-03-03 12:32:41 +0800

    Update tilawah.js

--- js/tilawah.js
+++ js/tilawah.js
@@ -1,13 +1,1 @@
-let tilawah = parseFloat(localStorage.getItem("tilawah")) || 0;
 
-function updateTilawah() {
-  document.getElementById("tilawahValue").innerText = tilawah;
-  localStorage.setItem("tilawah", tilawah);
-}
-
-function addTilawah(value) {
-  tilawah += value;
-  updateTilawah();
-}
-
-updateTilawah();
