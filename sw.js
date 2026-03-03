11015f54c5ccf10ccb025b62c438f3cdb125bfe3
rahmantaufiqu5-svg <rahmantaufiqu5@gmail.com>
2026-03-03 13:04:04 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,25 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("ramadhan-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/css/style.css",
-        "/js/main.js",
-        "/js/checklist.js",
-        "/js/tilawah.js",
-        "/js/theme.js"
-      ]);
-    })
+const cacheName = "ramadhan-companion-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/main.js",
+  "/js/checklist.js",
+  "/js/tilawah.js",
+  "/js/theme.js",
+  "/js/prayer.js",
+  "/js/muhasabah.js",
+  "/js/stats.js"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(cacheName).then(cache=>cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(resp=>resp||fetch(e.request))
   );
 });
