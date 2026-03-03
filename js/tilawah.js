05151988fb76ef4c3ed94e7b62aeafd2e8271af3
rahmantaufiqu5-svg <rahmantaufiqu5@gmail.com>
2026-03-03 12:42:41 +0800

    Update tilawah.js

--- js/tilawah.js
+++ js/tilawah.js
@@ -1,1 +1,8 @@
+let tilawahValue = 0;
+const tilawahDisplay = document.getElementById("tilawahValue");
 
+function addTilawah(amount) {
+  tilawahValue += amount;
+  if(tilawahValue < 0) tilawahValue = 0;
+  if(tilawahDisplay) tilawahDisplay.textContent = tilawahValue;
+}
